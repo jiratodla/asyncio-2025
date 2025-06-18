daad6bea9d83f38224058d8295f3b232d19e933b
jira todla <6620301004@cdti.ac.th>
2025-06-18 12:03:56 +0700

    thread_01

--- assignment02/thread01.py
+++ assignment02/thread01.py
@@ -1,1 +1,23 @@
 # Thread version of cooking 1 kitchen 1 chefs 1 dishes
+from time import sleep, ctime,time
+from threading import Thread
+
+def cooking(index):
+    start_time = time()
+    print(f'{ctime()}Kitchen-{index}  : Begin  cooking...')
+    sleep(2)
+    duration = time() - start_time
+    print(f'{ctime()}Kitchen-{index}  :     Cooking done in {duration:0.2f} seconds!')
+
+if __name__=="__main__":
+    print(f'{ctime()}Main     : Start Cooking.')
+    start_time = time()
+
+    index=1
+    c1 = Thread(target=cooking(index))
+    c1.start()
+    c1.join()
+
+    duration = time() - start_time
+    print(f"{ctime()}Main     :Finished Cooking duration in {duration:0.2f} seconds")
+    
