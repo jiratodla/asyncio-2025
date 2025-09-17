24421f1b9c42236f997ce19ab5d80e6972960d82
jira todla <6620301004@cdti.ac.th>
2025-09-16 21:08:24 -0700

    ee

--- exam-03/rocketme.py
+++ exam-03/rocketme.py
@@ -1,10 +1,11 @@
 import time
 
-student_id = "1234567890"
+student_id = "6620301004"
 
 async def fire_rocket(name: str, t0: float):
     url = f"http://172.16.2.117:8088/fire/{student_id}"
     start_time = time.perf_counter() - t0  # เวลาเริ่มสัมพัทธ์
+    
     """
     TODO:
     - ส่ง GET request ไปยัง rocketapp ที่ path /fire/{student_id}
@@ -21,7 +22,7 @@ async def fire_rocket(name: str, t0: float):
 
 async def main():
     t0 = time.perf_counter()  # เวลาเริ่มของชุด rockets
-
+    
     print("Rocket prepare to launch ...")  # แสดงตอนเริ่ม main
 
     # TODO: สร้าง task ยิง rocket 3 ลูกพร้อมกัน
