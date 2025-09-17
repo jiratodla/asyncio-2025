af89b18cef179b88f64033c0432ee9d57a6b3ee3
jira todla <6620301004@cdti.ac.th>
2025-09-16 20:28:05 -0700

    Ex.10

--- exam-01/examp10.py
+++ exam-01/examp10.py
@@ -24,7 +24,8 @@ async def main():
     tasks = []
     for i in range(3):
         tasks.append(asyncio.create_task(worker(f"Task-{i}", i+1)))
+    await asyncio.gather(*tasks)
+    print("Results:", [t.result() for t in tasks])
     
-    print("All tasks scheduled")
 
 asyncio.run(main())
